f79a1e7219bb7b8173a9339404f3ae150bea7d96
Gabrielorrico <agbo@cesar.school>
2026-08-30 15:53:29 -0300

    adicionando a funcao mandelbrot e a logica do calculo de cada par ordenado existente

--- agbo/src/main.c
+++ agbo/src/main.c
@@ -6,7 +6,7 @@
 int verificaEntrada(char args[],char argumento[]){
     char *endptr;
     int numConvertido = strtol(args,&endptr,10);
-    if(*endptr != "\0"){
+    if(*endptr != '\0'){
         fprintf(stderr,"ERRO: argumento %s escrito errado", argumento);
         exit(1);
     }
@@ -19,13 +19,68 @@ int verificaEntrada(char args[],char argumento[]){
     return numConvertido;
 }
 
+int mandelbrot(int maxIteracoes,double numComplexoReal, double numComplexoImag){
+    double zReal = 0;
+    double zImag = 0;
+    int qtdIteracoes = 0;
+    for(int k = 0; k < maxIteracoes; k++){
+
+        if(zImag*zImag + zReal*zReal > 4){
+            return k;
+        }
+        double temp = zReal;
+        double novozReal = temp*temp - zImag*zImag + numComplexoReal;
+        zReal = novozReal;
+        zImag = 2 * temp * zImag + numComplexoImag;
+        qtdIteracoes++;
+    }
+    return qtdIteracoes;
+}
+
 
 
 int main(int argc, char *argv[]) {
-
+    //./mandelbrot <altura> <largura> <maxIteracoes> <maxThreads>
     if(argc != 5){
         fprintf(stderr,"quantidade de argumentos invalida");
         exit(1);
+    }
+
+    int altura = verificaEntrada(argv[1],"altura");
+    int largura = verificaEntrada(argv[2],"largura");
+    int maxIteracoes = verificaEntrada(argv[3],"maxIteracoes");
+    int maxThreads = verificaEntrada(argv[4],"maxThreads");
+
+    int *matriz = (int*)malloc(sizeof(int) * altura * largura);
+    if(matriz == NULL){
+        fprintf(stderr,"ERRO: erro no malloc");
+    }
+
+    int xMenor =  -2;
+    int xMaior =  1;
+    int yMenor =  -1.5;
+    int yMaior =  1.5;
+
+    double larguraPixel = (xMaior - xMenor)/largura;
+    double alturaPixel = (yMaior - yMenor)/altura;
+
+
+    
+    for(int i = 0; i < altura; i++){//py
+        for(int j = 0; j < largura; j++){//px
+            double numComplexoReal = xMenor + j * larguraPixel;
+            double numComplexoImag = yMenor + i * alturaPixel;
+            int iteracoes = mandelbrot(maxIteracoes,numComplexoReal,numComplexoImag);
+            int indice = i*largura + j;
+            matriz[indice] = iteracoes;
+        }
+    }
+
+    for(int i =0; i<altura; i++){
+        for(int j = 0; j<largura; j++){
+            printf("%d ",matriz[i*largura + j]);
+        }
+        printf("\n");
     }
     
     return 0;
