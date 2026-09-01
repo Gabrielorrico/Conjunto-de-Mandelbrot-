7a0d49d26638a45ab8fdf4b0768db3c4c5a791b6
Gabrielorrico <agbo@cesar.school>
2026-09-01 12:39:13 -0300

    Adicionando o pthreads2 e corrigindo a escrita dos erros nos arquivos

--- agbo/src/main.c
+++ agbo/src/main.c
@@ -13,11 +13,28 @@ typedef struct thread{
     int maxIteracoes;
     int linhaInicial;
     int linhaFinal;
+    int numThreads;
     double xMenor;
     double yMenor;
     double larguraPixel;
     double alturaPixel;
 }thread;
+
+void* execThread2(void* arg){
+    thread *temp = (thread*)arg;
+
+    for(int i = temp->linhaInicial; i < temp->altura; i = i + temp->numThreads ){
+        for(int j = 0; j < temp->largura; j++){
+            double numComplexoReal = temp->xMenor + j * temp->larguraPixel;
+            double numComplexoImag = temp->yMenor + i * temp->alturaPixel;
+            int iteracoes = mandelbrot(temp->maxIteracoes,numComplexoReal,numComplexoImag);
+            int indice = i*temp->largura + j;
+            temp->matriz[indice] = iteracoes/(double)temp->maxIteracoes * 255;
+
+        }
+    }
+    return NULL;
+}
 
 void* execThread(void* arg){
     thread *temp = (thread*)arg;
@@ -37,15 +54,18 @@ void* execThread(void* arg){
 
 
 int verificaEntrada(char args[],char argumento[]){
+    FILE *filerro2 = NULL;
+    filerro2 = fopen("erros.txt","w");
+
     char *endptr;
     int numConvertido = strtol(args,&endptr,10);
     if(*endptr != '\0'){
-        fprintf(stderr,"ERRO: argumento %s escrito errado", argumento);
+        fprintf(filerro2,"ERRO: argumento %s escrito errado", argumento);
         exit(1);
     }
 
     if(numConvertido <= 0){
-        fprintf(stderr,"ERRO: argumento %s precisa ter um tamanho maior",argumento);
+        fprintf(filerro2,"ERRO: argumento %s precisa ter um tamanho maior",argumento);
         exit(1);
     }
 
@@ -73,9 +93,16 @@ int mandelbrot(int maxIteracoes,double numComplexoReal, double numComplexoImag){
 
 
 int main(int argc, char *argv[]) {
+    //arquivo de erros
+    FILE* filerro = NULL;
+    filerro = fopen("erros.txt","w");
+    if(filerro == NULL){
+        fprintf(stderr,"ERRO AO ABRIR O ARQUIVO DE ERRO");
+        exit(1);
+    }
     //./mandelbrot <altura> <largura> <maxIteracoes> <numThreads>
     if(argc != 5){
-        fprintf(stderr,"quantidade de argumentos invalida");
+        fprintf(filerro,"quantidade de argumentos invalida");
         exit(1);
     }
 
@@ -86,7 +113,8 @@ int main(int argc, char *argv[]) {
 
     int *matriz = (int*)malloc(sizeof(int) * altura * largura);
     if(matriz == NULL){
-        fprintf(stderr,"ERRO: erro no malloc");
+        fprintf(filerro,"ERRO: erro no malloc");
+        exit(1);
     }
 
     double xMenor =  -2;
@@ -116,7 +144,7 @@ int main(int argc, char *argv[]) {
 
     FILE *timesfileserial = fopen("times.txt","a");
     if(timesfileserial == NULL){
-        fprintf(stderr,"ERRO: erro ao abrir o arquivo");
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
         exit(1);
     }
     fprintf(timesfileserial, "serial: %f\n", tempo);
@@ -124,7 +152,7 @@ int main(int argc, char *argv[]) {
 
     FILE *arquivoserial = fopen("mandelbrot_agbo_serial.pgm","w");
     if(arquivoserial == NULL){
-        fprintf(stderr,"ERRO: erro ao abrir o arquivo");
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
         exit(1);
     }
 
@@ -159,7 +187,7 @@ int main(int argc, char *argv[]) {
 
     FILE *timesfileopenmp = fopen("times.txt","a");
     if(timesfileopenmp == NULL){
-        fprintf(stderr,"ERRO: erro ao abrir o arquivo");
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
         exit(1);
     }
     fprintf(timesfileopenmp, "openmp: %f\n", tempo);
@@ -167,7 +195,7 @@ int main(int argc, char *argv[]) {
 
     FILE *arquivoopenmp = fopen("mandelbrot_agbo_openmp.pgm","w");
     if(arquivoopenmp == NULL){
-        fprintf(stderr,"ERRO: erro ao abrir o arquivo");
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
         exit(1);
     }
 
@@ -186,12 +214,12 @@ int main(int argc, char *argv[]) {
     thread *threads = (thread*)malloc(sizeof(thread) * numThreads);
 
     if(ids == NULL){
-        fprintf(stderr,"ERRO: erro ao alocar memoria para os ids dos Threads");
+        fprintf(filerro,"ERRO: erro ao alocar memoria para os ids dos Threads");
         exit(1);
     }
 
     if(threads == NULL){
-        fprintf(stderr,"ERRO: erro ao alocar memoria para o vetor da struct de threads");
+        fprintf(filerro,"ERRO: erro ao alocar memoria para o vetor da struct de threads");
         exit(1);
     }
 
@@ -223,7 +251,7 @@ int main(int argc, char *argv[]) {
 
     FILE *timesfilepthreads1 = fopen("times.txt","a");
     if(timesfilepthreads1 == NULL){
-        fprintf(stderr,"ERRO: erro ao abrir o arquivo");
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
         exit(1);
     }
     fprintf(timesfilepthreads1, "pthreads1: %f\n", tempo);
@@ -232,20 +260,90 @@ int main(int argc, char *argv[]) {
     free(ids);
     free(threads);
 
-    FILE *arquivopthreads = fopen("mandelbrot_agbo_pthreads1.pgm","w");
-    if(arquivopthreads == NULL){
+    FILE *arquivopthreads1 = fopen("mandelbrot_agbo_pthreads1.pgm","w");
+    if(arquivopthreads1 == NULL){
         fprintf(stderr,"ERRO: erro ao abrir o arquivo");
         exit(1);
     }
 
     for(int i = 0; i < altura; i++){
         for(int j = 0; j < largura; j++){
-            fprintf(arquivopthreads,"%d ",matriz[i*largura + j]);
+            fprintf(arquivopthreads1,"%d ",matriz[i*largura + j]);
         }
-        fprintf(arquivopthreads,"\n");
+        fprintf(arquivopthreads1,"\n");
     }
 
-    fclose(arquivopthreads);
+    fclose(arquivopthreads1);
+    //fim pthread1
+    //iniciopthread2
     
+    ids = (pthread_t*)malloc(sizeof(pthread_t) * numThreads);
+    threads = (thread*)malloc(sizeof(thread) * numThreads);
+
+    if(ids == NULL){
+        fprintf(stderr,"ERRO: erro ao alocar memoria para os ids dos Threads");
+        exit(1);
+    }
+
+    if(threads == NULL){
+        fprintf(filerro,"ERRO: erro ao alocar memoria para o vetor da struct de threads");
+        exit(1);
+    }
+
+    clock_gettime(CLOCK_MONOTONIC,&inicio);
+    
+    for(int t = 0; t < numThreads; t++){
+        threads[t].numThreads = numThreads;
+        threads[t].altura = altura;
+        threads[t].largura = largura;
+        threads[t].xMenor = xMenor;
+        threads[t].yMenor = yMenor;
+        threads[t].maxIteracoes = maxIteracoes;
+        threads[t].larguraPixel = larguraPixel;
+        threads[t].alturaPixel = alturaPixel;
+        threads[t].linhaInicial = t;
+        threads[t].linhaFinal = altura/numThreads * (t+1);
+        threads[t].matriz = matriz;
+        pthread_create(&ids[t],NULL,execThread2,&threads[t]);
+
+    }
+
+    for(int t = 0; t < numThreads; t++){
+        pthread_join(ids[t],NULL);
+
+    }
+
+    clock_gettime(CLOCK_MONOTONIC,&fim);
+
+    tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) * 0.000000001;
+
+    FILE *timesfilepthreads2 = fopen("times.txt","a");
+    if(timesfilepthreads2 == NULL){
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
+        exit(1);
+    }
+    fprintf(timesfilepthreads2, "pthreads2: %f\n", tempo);
+    fclose(timesfilepthreads2);
+
+    free(ids);
+    free(threads);
+
+    FILE *arquivopthreads2 = fopen("mandelbrot_agbo_pthreads2.pgm","w");
+    if(arquivopthreads2 == NULL){
+        fprintf(filerro,"ERRO: erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    for(int i = 0; i < altura; i++){
+        for(int j = 0; j < largura; j++){
+            fprintf(arquivopthreads2,"%d ",matriz[i*largura + j]);
+        }
+        fprintf(arquivopthreads2,"\n");
+    }
+
+    fclose(arquivopthreads2);
+
+    //fimpthread2
+    fclose(filerro);
     return 0;
 }
